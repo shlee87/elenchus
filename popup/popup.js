35039b91ae4f0fba0f53f7089a475b1db081ec89
SeonghoonYi <nks4860@gmail.com>
2023-07-24 16:25:25 -0500

    working on login feature

---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+document.getElementById('submit').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Do something with the username and password
+    console.log('Username:', username);
+    console.log('Password:', password);
+});
